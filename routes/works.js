74f00fcd44b1630a0bd316455b1b8c3677ed46d6
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2023-01-08 13:37:58 -0500

    go manuscript route for works page

--- routes/works.js
+++ routes/works.js
@@ -1,9 +1,24 @@
+var fs = require('fs')
 var express = require('express')
+var parser = require('../utils/parser')
 
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.status(200).render('main/works')
+  var filePath = 'manuscripts/main/works.txt'
+  fs.readFile(filePath, 'utf8', function (err, manuscript) {
+    if (err) {
+      res.status(500).render('error/500')
+    } else {
+      try {
+        var path = `${req.baseUrl}${req.path}`
+        var result = parser.parse(path, manuscript)
+        res.status(200).type('text/html').send(result.html)
+      } catch (err) {
+        res.status(500).render('error/500')
+      }
+    }
+  })
 })
 
 module.exports = router
