58dfc67a3232126a1122292576b99e14e617450e
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2024-10-05 00:39:44 -0400

    experimental animations with home page images

---
+++ public/js/p5js/home.js
@@ -0,0 +1,105 @@
+p5.disableFriendlyErrors = true;
+
+var WIDTH;
+var HEIGHT;
+var IMAGE;
+
+class Transform {
+    constructor() { }
+
+    setup() {
+        throw new Error("not implemented");
+    }
+
+    draw() {
+        throw new Error("not implemented");
+    }
+}
+
+class Inverter extends Transform {
+    constructor() {
+        super();
+        this.step = 0
+    }
+
+    setup() {
+        frameRate(1);
+    }
+
+    draw() {
+        frameCount % 10 == 0 ? image(IMAGE, 0, 0) : undefined;
+
+        let dx = random(0.25, 0.75) * WIDTH;
+        let dy = random(0.25, 0.75) * HEIGHT;
+        let x = random(0, WIDTH - dx);
+        let y = random(0, HEIGHT - dy);
+
+        let piece = get(x, y, dx, dy);
+        piece.filter(INVERT);
+
+        set(x, y, piece);
+    }
+}
+
+class Glitcher extends Transform {
+    constructor() {
+        super();
+        this.glitch = new Glitch();
+    }
+
+    setup() {
+        this.glitch.loadImage(IMAGE);
+    }
+
+    draw() {
+        this.glitch.resetBytes();
+
+        this.glitch.randomBytes(2); // add one random byte for movement
+
+        this.glitch.buildImage();
+        image(this.glitch.image, 0, 0)
+    }
+}
+
+
+function getImageData() {
+    const img = document.querySelector('figure img');
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(
+        img,
+        0, 0, img.naturalWidth, img.naturalHeight, // src
+        0, 0, canvas.width, canvas.height, // dst
+    );
+
+    return canvas.toDataURL();
+}
+
+function preload() {
+    const imageData = getImageData();
+    loadImage(imageData, (img) => {
+        IMAGE = img;
+    });
+}
+
+function setupCanvas() {
+    WIDTH = IMAGE.width;
+    HEIGHT = IMAGE.height;
+    createCanvas(WIDTH, HEIGHT);
+    image(IMAGE, 0, 0);
+}
+
+function setup() {
+    setupCanvas();
+    C = random([Inverter, Glitcher]);
+    sys = new C();
+    sys.setup();
+}
+
+function draw() {
+    sys.draw();
+}
--- routes/home.js
+++ routes/home.js
@@ -19,15 +19,16 @@ var IMAGES = [
   '/media/images/storm_king/storm-king-path-5.png',
 
   // my images hosted elsewhere
-  'https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/356/680/545/068/533/original/504dc756fa36dc8d.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/940/996/029/241/076/original/acb9de6c78e2011e.jpg'
+  // FIXME: running into CORS issues when trying to load image data for these
+  // 'https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg',
+  // 'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg',
+  // 'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/356/680/545/068/533/original/504dc756fa36dc8d.jpg',
+  // 'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/940/996/029/241/076/original/acb9de6c78e2011e.jpg'
 ]
 
 router.get('/', function (req, res, next) {
