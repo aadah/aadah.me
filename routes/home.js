6f8e15626de44c3e54e4c640ab328fe23b2f2547
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2024-11-27 14:12:22 -0500

    fix: CORS workaround with proxy

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
-var express = require('express')
-var parser = require('../utils/parser')
+const express = require('express')
+const parser = require('../utils/parser')
+const axios = require('axios')
 
 var router = express.Router()
 
-var IMAGES = [
+var INTERNAL = [
   // my locally hosted images
 
   '/media/images/bbg-lilies.png',
@@ -18,7 +19,9 @@ var IMAGES = [
   '/media/images/lebanon_2022/byblos_souk.png',
   '/media/images/stata.png',
   '/media/images/storm_king/storm-king-path-5.png',
+]
 
+var EXTERNAL = new Set([
   // my images hosted elsewhere
 
   'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/356/680/545/068/533/original/504dc756fa36dc8d.jpg',
@@ -28,21 +31,53 @@ var IMAGES = [
   'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/110/666/002/908/845/original/48cb1f949e1e38b6.jpg',
   'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/232/305/071/999/581/original/59613bf0511a122c.jpg',
   'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/280/692/722/680/618/original/9fbdedb4ec83b9fb.jpg',
-  
-  // FIXME: running into CORS issues when trying to load image data for these
-  '/media/images/backyard-garden.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg',
-  '/media/images/bajaj.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg',
-  '/media/images/guitar-living-room.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg',
-  '/media/images/pueblito-paisa.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg',
-  '/media/images/wall-somalia.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg',
-  '/media/images/cali-jimale-masjid-inside.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg',
-  '/media/images/cali-jimale-masjid-outside.jpg', // 'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg',
-]
+
+  'https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg',
+  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg',
+])
+
+EXTERNAL.forEach((url) => {
+  INTERNAL.push(`/proxy/${encodeURIComponent(url)}`);
+})
 
 router.get('/', function (req, res, next) {
-  var index = Math.floor(Math.random() * IMAGES.length)
-  var image = IMAGES[index]
+  var index = Math.floor(Math.random() * INTERNAL.length)
+  var image = INTERNAL[index]
   parser.web.handler('manuscripts/main/home.txt', { 'IMAGE': image })(req, res, next)
 })
+
+router.get('/proxy/:url(*)', (req, res) => {
+  const targetUrl = new URL(decodeURIComponent(req.params.url));
+
+  // use the proxy only for allowed external resources
+  if (!EXTERNAL.has(targetUrl.toString())) {
+    res.status(404).render('error/404')
+    return
+  }
+
+  // set the target host for the request
+  req.headers['host'] = targetUrl.hostname
+
+  axios.get(targetUrl, {
+    responseType: 'stream',
+    headers: req.headers, // otherwise use identical headers
+  }).then(function (response) {
+    // forward response headers
+    response.headers && Object.entries(response.headers).forEach(([key, value]) => {
+      res.set(key, value);
+    });
+
+    // forward data
+    response.data.pipe(res);
+  }).catch(function (err) {
+    const status = err.response?.status || 500;
+    res.status(status).render(`error/${status}`)
+  });
+});
 
 module.exports = router
