0c95513dbe75eda5dc0df4101475566d517ccbf5
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2026-05-10 22:26:26 -0400

    new photos

--- routes/home.js
+++ routes/home.js
@@ -1,89 +1,106 @@
-const express = require('express')
-const parser = require('../utils/parser')
-const axios = require('axios')
+const express = require("express");
+const parser = require("../utils/parser");
+const axios = require("axios");
 
-var router = express.Router()
+var router = express.Router();
 
 var INTERNAL = [
   // my locally hosted images
 
-  '/media/images/bbg-lilies.png',
-  '/media/images/blm_protest/blm_protest_1.png',
-  '/media/images/brooklyn-botanic-2.png',
-  '/media/images/children.jpg',
-  '/media/images/fireworks.jpg',
-  '/media/images/great-diamond-island-bunker-wall.png',
-  '/media/images/harvard_stadium_bleachers.png',
-  '/media/images/himejijou.JPG',
-  '/media/images/lebanon_2022/byblos_mountain_path_1.png',
-  '/media/images/lebanon_2022/byblos_souk.png',
-  '/media/images/stata.png',
-  '/media/images/storm_king/storm-king-path-5.png',
-]
+  "/media/images/bbg-lilies.png",
+  "/media/images/blm_protest/blm_protest_1.png",
+  "/media/images/brooklyn-botanic-2.png",
+  "/media/images/children.jpg",
+  "/media/images/fireworks.jpg",
+  "/media/images/great-diamond-island-bunker-wall.png",
+  "/media/images/harvard_stadium_bleachers.png",
+  "/media/images/himejijou.JPG",
+  "/media/images/lebanon_2022/byblos_mountain_path_1.png",
+  "/media/images/lebanon_2022/byblos_souk.png",
+  "/media/images/stata.png",
+  "/media/images/storm_king/storm-king-path-5.png",
+];
 
 var EXTERNAL = new Set([
   // my images hosted elsewhere
 
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/356/680/545/068/533/original/504dc756fa36dc8d.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/588/592/380/643/872/original/4a088887cdf6e288.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/940/996/029/241/076/original/acb9de6c78e2011e.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/071/358/899/961/113/original/abc22e03b1a4c4c7.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/110/666/002/908/845/original/48cb1f949e1e38b6.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/232/305/071/999/581/original/59613bf0511a122c.jpg',
-  'https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/280/692/722/680/618/original/9fbdedb4ec83b9fb.jpg',
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/356/680/545/068/533/original/504dc756fa36dc8d.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/588/592/380/643/872/original/4a088887cdf6e288.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/112/940/996/029/241/076/original/acb9de6c78e2011e.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/071/358/899/961/113/original/abc22e03b1a4c4c7.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/110/666/002/908/845/original/48cb1f949e1e38b6.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/232/305/071/999/581/original/59613bf0511a122c.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/280/692/722/680/618/original/9fbdedb4ec83b9fb.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/115/721/047/197/219/197/original/05845d1197764188.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/115/799/990/024/437/054/original/fd3dcba88a83f4ef.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/115/721/047/197/219/197/original/05845d1197764188.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/115/799/990/024/437/054/original/fd3dcba88a83f4ef.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/114/577/765/159/995/514/original/5f1817e66deedd0d.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/488/121/965/113/443/original/000bc1c5a9d90645.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/365/487/918/211/054/original/ce1c2f6e21a7e97c.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/488/121/942/652/070/original/4b05425bda0901df.jpg",
+  "https://social-coop-media.ams3.cdn.digitaloceanspaces.com/media_attachments/files/113/487/998/343/335/013/original/b71a72a2fa2d8f19.jpg",
 
-  'https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg',
-  'https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg',
-])
+  "https://mastodon.mit.edu/system/media_attachments/files/110/952/813/571/606/256/original/3e9f841de5d6b8c3.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/111/651/094/839/514/862/original/1b0d6a736db8df0a.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/111/879/362/235/552/285/original/80c4dd1bad1e455d.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/110/682/808/712/825/718/original/ef256851bdfce2d0.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/111/682/295/720/395/669/original/943372f64c2d7b86.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/111/705/098/563/938/086/original/f30e067d24df8105.jpg",
+  "https://mastodon.mit.edu/system/media_attachments/files/111/705/098/191/567/569/original/48e9bc2268d6014d.jpg",
+]);
 
 var PROXY_WHITELIST = new Set([
   // tech for palestine banner
-  'https://banner.techforpalestine.org/lib/banner.min.js'
-])
+  "https://banner.techforpalestine.org/lib/banner.min.js",
+]);
 
 EXTERNAL.forEach((url) => {
   INTERNAL.push(`/proxy/${encodeURIComponent(url)}`);
-  PROXY_WHITELIST.add(url)
-})
+  PROXY_WHITELIST.add(url);
+});
 
-router.get('/', function (req, res, next) {
-  var index = Math.floor(Math.random() * INTERNAL.length)
-  var image = INTERNAL[index]
-  parser.web.handler('manuscripts/main/home.txt', { 'IMAGE': image })(req, res, next)
-})
+router.get("/", function (req, res, next) {
+  var index = Math.floor(Math.random() * INTERNAL.length);
+  var image = INTERNAL[index];
+  parser.web.handler("manuscripts/main/home.txt", { IMAGE: image })(
+    req,
+    res,
+    next,
+  );
+});
 
-router.get('/proxy/:url(*)', (req, res) => {
+router.get("/proxy/:url(*)", (req, res) => {
   const targetUrl = new URL(decodeURIComponent(req.params.url));
 
   // use the proxy only for allowed external resources
   if (!PROXY_WHITELIST.has(targetUrl.toString())) {
-    res.status(404).render('error/404')
-    return
+    res.status(404).render("error/404");
+    return;
   }
 
   // set the target host for the request
-  req.headers['host'] = targetUrl.hostname
+  req.headers["host"] = targetUrl.hostname;
 
-  axios.get(targetUrl, {
-    responseType: 'stream',
-    headers: req.headers, // otherwise use identical headers
-  }).then(function (response) {
-    // forward response headers
-    response.headers && Object.entries(response.headers).forEach(([key, value]) => {
-      res.set(key, value);
+  axios
+    .get(targetUrl, {
+      responseType: "stream",
+      headers: req.headers, // otherwise use identical headers
+    })
+    .then(function (response) {
+      // forward response headers
+      response.headers &&
+        Object.entries(response.headers).forEach(([key, value]) => {
+          res.set(key, value);
+        });
+
+      // forward data
+      response.data.pipe(res);
+    })
+    .catch(function (err) {
+      const status = err.response?.status || 500;
+      res.status(status).render(`error/${status}`);
     });
-
-    // forward data
-    response.data.pipe(res);
-  }).catch(function (err) {
-    const status = err.response?.status || 500;
-    res.status(status).render(`error/${status}`)
-  });
 });
 
-module.exports = router
+module.exports = router;
